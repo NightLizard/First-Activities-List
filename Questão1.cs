8cd3650ae0c21ed5366320ace53f16a7d8fbad8b
Péricles <55357886+NightLizard@users.noreply.github.com>
2019-09-15 14:42:46 -0300

    Add files via upload

---
+++ Questão1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Informe um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            {
+                if (num < 0)
+                    Console.WriteLine("Número é negativo");
+                else if (num > 0 )
+                    Console.WriteLine("Número é positivo");
+                else
+                    Console.WriteLine("Número é nulo");
+            }
+            Console.ReadKey();
+        }
+    }
+}
