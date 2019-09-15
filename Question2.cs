27aa61b38c40d7ff53910456b26cf7fa6f27ea3b
Péricles <55357886+NightLizard@users.noreply.github.com>
2019-09-15 16:43:55 -0300

    Add files via upload

---
+++ Question2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Informe um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            {
+                if (num % 2 == 0)
+                    Console.WriteLine("Número é par");
+                else
+                    Console.WriteLine("Número é ímpar");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
